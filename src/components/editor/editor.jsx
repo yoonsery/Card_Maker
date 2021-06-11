1cae42fb538059e304bfee8b25a7e3c86ffe38b4
yoonsery <yoon.sery@icloud.com>
2021-06-12 01:06:20 +0900

    add card & edit form component

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './editor.module.css';
+import CardEditForm from '../card_edit_form/card_edit_form';
 
-const Editor = (props) => {
+const Editor = ({ cards }) => {
   return (
     <section className={styles.editor}>
       <h1 className={styles.title}>Card Maker</h1>
+      {cards.map((card) => (
+        <CardEditForm card={card} />
+      ))}
     </section>
   );
 };
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,12 +1,51 @@
-import { React, useEffect } from 'react';
+import { React, useEffect, useState } from 'react';
 import styles from './maker.module.css';
 import Header from '../header/header';
 import Footer from '../footer/footer';
 import { useHistory } from 'react-router-dom';
 import Editor from '../editor/editor';
 import Preview from '../preview/preview';
+import Cards from '../cards/cards';
+
+const { user } = new Cards();
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Ellie Dream Coding',
+      company: 'Spotify',
+      theme: 'dark',
+      title: 'Software Engineer',
+      email: 'dream.coder@gmail.com',
+      message: "Don't forget to code your dream",
+      fileName: 'ellie',
+      fileURL: null,
+    },
+    {
+      id: '2',
+      name: 'Bob',
+      company: 'Uber',
+      theme: 'light',
+      title: 'Senior Software Engineer',
+      email: 'bob@uber.com',
+      message: 'Go for it',
+      fileName: 'bob',
+      fileURL: null,
+    },
+    {
+      id: '3',
+      name: 'Chris',
+      company: 'Instagram',
+      theme: 'colorful',
+      title: 'Product Manager',
+      email: 'chris@instagram.com',
+      message: 'Design your dream',
+      fileName: 'chris',
+      fileURL: null,
+    },
+  ]);
+
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -23,8 +62,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
