a4acde4b31ba3a9c60c922df971519950573cd78
yoonsery <yoon.sery@icloud.com>
2021-06-11 19:42:11 +0900

    add card editor & preview layout

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -3,6 +3,8 @@ import styles from './maker.module.css';
 import Header from '../header/header';
 import Footer from '../footer/footer';
 import { useHistory } from 'react-router-dom';
+import Editor from '../editor/editor';
+import Preview from '../preview/preview';
 
 const Maker = ({ authService }) => {
   const history = useHistory();
@@ -20,7 +22,10 @@ const Maker = ({ authService }) => {
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
-
+      <div className={styles.container}>
+        <Editor />
+        <Preview />
+      </div>
       <Footer />
     </section>
   );
--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -3,7 +3,7 @@ import firebaseApp from './firebase';
 
 class AuthService {
   login(providerName) {
-    const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
+    const authProvider = new firebase.auth[`${providerName}AuthProvider`](); // key 값 읽을 때 obj.key 말고 obj['key']로 읽어오는 거랑 같음
     return firebaseApp.auth().signInWithPopup(authProvider);
   }
 
